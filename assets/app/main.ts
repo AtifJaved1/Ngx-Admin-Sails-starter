e5e6e73f161d2c6e50d146304e88c6f9c41eceaa
Atif Javed <ddp-sp14-bcs-012@ciitlahore.edu.pk>
2018-02-17 16:54:46 +0500

    First Commit

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -1,7 +1,12 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -9,4 +14,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
