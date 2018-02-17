83b9aa90a963c131565bb41540ea86ecf99d4eb0
Atif Javed <ddp-sp14-bcs-012@ciitlahore.edu.pk>
2018-02-17 18:46:12 +0500

    New Commit

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/components/home/home.component';
 
 @NgModule({
   declarations: [
